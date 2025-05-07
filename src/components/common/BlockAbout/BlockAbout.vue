952618160d6353ecb02e46bcb6e65fa6d1dd7a41
HellgaProkopovich <hellgamur@mail.ru>
2025-05-07 18:56:03 +0300

    feat: BaseTitlesContainer & BlockAbout

--- src/components/common/BlockAbout/BlockAbout.vue
+++ src/components/common/BlockAbout/BlockAbout.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="block-about"></div>
+  <div class="block-about">
+    <BaseTitlesContainer
+      title="Уникален не я, а мой опыт"
+      subtitle="[Обо мне]"
+      align="left"
+    />
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non aut dolorum eveniet culpa qui. Aliquid voluptatem, libero, eveniet maxime culpa mollitia iure ipsam nesciunt sint dolorum, illum inventore saepe id.</div>
+  </div>
 </template>
 
 <script>
+// import BaseTitlesContainer from '@/components/ui/BaseTitlesContainer/BaseTitlesContainer.vue';
 export default {
   name: 'BlockAbout',
+  // components: {
+  //   BaseTitlesContainer
+  // }
 }
+
 </script>
 
-<style lang="postcss">
+<style>
 .block-about {
+  width: 100vw;
 }
 </style>
---
+++ src/components/ui/BaseTitlesContainer/BaseTitlesContainer.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- <Component
+    :is="div"
+    class="titles-container"
+    :class=[alignTitles]
+  ></Component> -->
+
+  <div :class="['titles-container', alignTitlesContClass]">
+    <h3 class="heading-thirdly">{{ subtitle }}</h3>
+    <h2 class="heading-secondary">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseTitlesContainer',
+  props: {
+    title: String,
+    subtitle: String,
+    align: {
+      type: String,
+      default: 'left',
+      validator: (value) => ['left', 'right'].includes(value),
+    }
+  },
+  computed: {
+    alignTitlesContClass () {
+      return this.align ===  'right'
+      ? 'titles-container_align-right'
+      : 'titles-container_align-left'
+    }
+  }
+}
+// defineProps({
+//   title: String,
+//   subtitle: String,
+//   align: {
+//     type: String,
+//     default: 'left',
+//     validator: (value) => ['left', 'right'].includes(value),
+//   }
+// })
+
+// const alignTitlesContainer = {
+//   left: 'titles-container_align-left',
+//   right: 'titles-container_align-right'
+// }[align]
+</script>
+
+<style>
+.titles-container {
+  padding: 10px;
+}
+.titles-container_align-left {
+  text-align: left;
+}
+.titles-container_align-right {
+  text-align: right;
+}
+</style>
